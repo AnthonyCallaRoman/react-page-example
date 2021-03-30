1ca7fe1d3cdb72266d64b82b74bbc76ef1ec5dc5
Anthony Calla <55769459+AnthonyCallaRoman@users.noreply.github.com>
2021-03-30 09:57:36 -0500

    Primer comit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Application - This is my application
         </p>
         <a
           className="App-link"
